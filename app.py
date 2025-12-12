37238c6b957a281f0086018d99058bd8324edb44
Timo <timo@timoq.com>
2025-12-12 18:26:08 +0800

    update listen port 5002

--- app.py
+++ app.py
@@ -680,4 +680,4 @@ def query_ip2region(ip):
         return {'error': str(e)}
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5002)
